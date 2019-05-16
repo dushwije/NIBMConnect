a277bb50a2406169cefa28bf5de0002a06312197
DushmaWijesinghe <dushmawijesinghe14@gmail.com>
2019-05-16 06:38:30 +0530

    login screen created

---
+++ NIBMConnect/ViewControllers/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  NIBMConnect
+//
+//  Created by Dushma Wijesinghe on 5/16/19.
+//  Copyright © 2019 Dushma Wijesinghe. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
